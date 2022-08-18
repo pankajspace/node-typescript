d8c832a1f9debed8df290f3f48e1726be61ac3fa
Pankaj Wakchaure <pankaj.corp@gmail.com>
2022-08-18 13:50:59 +0530

    initial setup completed

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ install postman
 
 npm i ts-node --save
 npm i nodemon --save
+npm i typescript --save
+npx tsc --init
+npm i @types/node --save
+
+in .tsconfig.json file make
+  "target": "es2017",
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const msg: string = "Hello World!";
+console.log(msg);
+
+const printNumbers = (start: number, end: number): void => {
+  if (start < end) {
+    while (start <= end) {
+      console.log(start);
+      start++;
+    }
+  } else {
+    console.log("Please enter start number less than end number");
+  }
+};
+
+printNumbers(1, 10);
