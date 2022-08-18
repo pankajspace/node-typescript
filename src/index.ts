06e277a54ff2724f179197f920cd8a89eee9e9bb
Pankaj Wakchaure <pankaj.corp@gmail.com>
2022-08-18 19:59:12 +0530

    StringUtil module created

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import StringUtilities, { getStringLength } from "./util/StringUtil";
+
+const str: string = "Hello World!";
+console.log("1. Length is: ", StringUtilities.getLength(str));
+console.log("2. Length is: ", getStringLength(str));
---
+++ src/util/StringUtil.ts
@@ -0,0 +1,11 @@
+class StringUtilities {
+  static getLength(str: string): number {
+    return str.length;
+  }
+}
+
+export function getStringLength(str: string): number {
+  return str.length;
+}
+
+export default StringUtilities;
